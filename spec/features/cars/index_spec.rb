9901fbaf76c717c2af4ee9b25dfe52a1b82dd86d
Clyde <clyde.autin@gmail.com>
2024-05-09 13:33:34 -0700

    first user story now passes

---
+++ spec/features/cars/index_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "Cars Index Page", type: :feature do
+    # [ ] done
+
+    # User Story 1, Parent Index 
+    
+    # For each parent table
+    # As a visitor
+    # When I visit '/parents'
+    # Then I see the name of each parent record in the system
+
+    describe "As a Visitor" do
+        describe "When I visit cars index" do
+            it "I can see all the cars model and manufacturer" do
+                supra = Car.create!(manufacturer: "Toyota", 
+                                    model: "GR Supra 3.0", 
+                                    base_msrp: "55400", 
+                                    manual_option: true, category: "Sports Car")
+                visit "/cars"
+
+                expect(page).to have_content("Toyota")
+                expect(page).to have_content("GR Supra 3.0")
+            end
+        end
+    end
+end
